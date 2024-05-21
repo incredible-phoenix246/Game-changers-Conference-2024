d86dc6bcf77e04188eb927fbcba705d08f666b5e
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-05-21 14:01:46 +0100

    open grrap

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { AboutHero, AboutUs } from "@/modules/about";
 import LogoCarousel from "@/components/slider";
 import { ConfStat } from "@/modules/underHero";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "ABOUT OGUN DIGITAL",
+};
 
 const page = () => {
   return (
